1d13b8453c347dbef62bf001841d8a43cc4f4c19
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-13 21:45:50 +0200

    Add brands section

--- src/components/brand/Brand.js
+++ src/components/brand/Brand.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import './brand.scss'
-import '../../scss/_utils.scss'
+import React from 'react';
+import './brand.scss';
+import '../../scss/_utils.scss';
+import { google, slack, atlassian, shopify, dropbox } from './imports';
 
 function Brand() {
   return (
-    <div>Brand</div>
-  )
+    <div className='brands flex'>
+      <img src={google} alt='google' />
+      <img src={slack} alt='slack' />
+      <img src={atlassian} alt='atlassian' />
+      <img src={dropbox} alt='dropbox' />
+      <img src={shopify} alt='shopify' />
+    </div>
+  );
 }
 
-export default Brand
+export default Brand;
