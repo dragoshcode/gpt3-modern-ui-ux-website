cd61116aa9e482499f08c800cb57688965f61f52
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-13 16:44:44 +0200

    Style nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import './navbar.scss'
-import '../../scss/_utils.scss'
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+import './navbar.scss';
+import '../../scss/_utils.scss';
+
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className='gpt3__navbar flex'>
+      <div className='gpt3__navbar-links flex'>
+        <img src={logo} alt='logo' />
+        <a href='/'>Home</a>
+        <a href='/'>What is GPT?</a>
+        <a href='/'>Open AI</a>
+        <a href='/'>Case Studies</a>
+        <a href='/'>Library</a>
+      </div>
+      <div className='gpt3__navbar-btns flex'>
+        <button className='sign-in'>Sign In</button>
+        <button className='sign-up'>Sign Up</button>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
