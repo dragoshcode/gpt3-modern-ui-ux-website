df0f76cd87abdbe668911433afa5010762b52ff5
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-14 16:10:50 +0200

    Add markup for 'Blogs' section

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,11 +1,18 @@
-import React from 'react'
-import './article.scss'
-import '../../scss/_utils.scss'
+import React from 'react';
+import './article.scss';
+import '../../scss/_utils.scss';
 
-function Article() {
+function Article({ imgUrl }) {
   return (
-    <div>Article</div>
-  )
+    <>
+      <img src={imgUrl} alt='blog' />
+      <div className='blog__card-data'>
+        <p>Sep 26, 2021</p>
+        <h2>GPT-3 and Open AI is the future. Let us exlore how it is?</h2>
+      </div>
+      <a href='/'>Read Full Article</a>
+    </>
+  );
 }
 
-export default Article
+export default Article;
--- src/containers/blog/Blog.js
+++ src/containers/blog/Blog.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import './blog.scss'
-import '../../scss/_utils.scss'
+import React from 'react';
+import './blog.scss';
+import '../../scss/_utils.scss';
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className='blog'>
+      <div className='blog__card'>
+        <Article imgUrl={blog01}/>
+      </div>
+      <div className='blog__card'>
+        <Article imgUrl={blog02}/>
+      </div>
+      <div className='blog__card'>
+        <Article imgUrl={blog03}/>
+      </div>
+      <div className='blog__card'>
+        <Article imgUrl={blog04}/>
+      </div>
+      <div className='blog__card'>
+        <Article imgUrl={blog05}/>
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
