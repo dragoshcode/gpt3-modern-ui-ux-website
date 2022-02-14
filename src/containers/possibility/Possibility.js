d01f4805888f37f95acc1e041cfec1ae033c74ab
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-14 14:15:16 +0200

    Add 'Possibility' section markup and styles for mobile view

--- src/containers/possibility/Possibility.js
+++ src/containers/possibility/Possibility.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './possibility.scss'
 import '../../scss/_utils.scss'
+import possibilityImage from '../../assets/possibility.png'
+
 function Possibility() {
   return (
-    <div>Possibility</div>
+    <div className='possibility grid'>
+      <div className="possibility__image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="possibility__content flex-col">
+        <h2 className='gradient-text'>The possibilities are beyond your imagination</h2>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        {/* make this button blue, not orange */}
+        <button>Request Early Access to Get Started</button>
+      </div>
+    </div>
   )
 }
 
