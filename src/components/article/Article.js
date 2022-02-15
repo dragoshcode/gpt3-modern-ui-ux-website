584c173be326f8d7254f3dc25cd46fbb63f815e0
Dragosh Gheceanu <dragoshcode@gmail.com>
2022-02-15 13:05:41 +0200

    Style 'Blogs' section

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -6,11 +6,13 @@ function Article({ imgUrl }) {
   return (
     <>
       <img src={imgUrl} alt='blog' />
-      <div className='blog__card-data'>
-        <p>Sep 26, 2021</p>
-        <h2>GPT-3 and Open AI is the future. Let us exlore how it is?</h2>
+      <div className='blog__card-content flex-col'>
+        <div className='blog__card-content-description'>
+          <p>Sep 26, 2021</p>
+          <h2>GPT-3 and Open AI is the future. Let us explore how it is?</h2>
+        </div>
+        <a href='/'>Read Full Article</a>
       </div>
-      <a href='/'>Read Full Article</a>
     </>
   );
 }
--- src/containers/blog/Blog.js
+++ src/containers/blog/Blog.js
@@ -6,21 +6,26 @@ import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 
 function Blog() {
   return (
-    <div className='blog'>
-      <div className='blog__card'>
-        <Article imgUrl={blog01}/>
-      </div>
-      <div className='blog__card'>
-        <Article imgUrl={blog02}/>
-      </div>
-      <div className='blog__card'>
-        <Article imgUrl={blog03}/>
-      </div>
-      <div className='blog__card'>
-        <Article imgUrl={blog04}/>
-      </div>
-      <div className='blog__card'>
-        <Article imgUrl={blog05}/>
+    <div className='blog-wrapper'>
+      <h2 className='gradient-text'>
+        A lot is happening, We are blogging about it.
+      </h2>
+      <div className='blog grid'>
+        <div className='blog__card human'>
+          <Article imgUrl={blog02} />
+        </div>
+        <div className='blog__card chain'>
+          <Article imgUrl={blog03} />
+        </div>
+        <div className='blog__card binary'>
+          <Article imgUrl={blog04} />
+        </div>
+        <div className='blog__card bang'>
+          <Article imgUrl={blog05} />
+        </div>
+        <div className='blog__card tunnel span-two'>
+          <Article imgUrl={blog01} />
+        </div>
       </div>
     </div>
   );
